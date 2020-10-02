25cbb8064b05658f54a1ccc5d992fd8f0b511b59
Adel Mandanas <mandanas.adel@gmail.com>
2020-10-02 20:39:34 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+const App: FunctionComponent = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
