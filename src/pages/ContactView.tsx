9473ae31793245c99a0c4ec373433bf991a73554
Celine Moredo <camoredo@gmail.com>
2020-10-04 19:53:17 +0800

    Update contact and faq page

--- src/pages/ContactView.tsx
+++ src/pages/ContactView.tsx
@@ -1,14 +1,40 @@
 import React, { FunctionComponent } from 'react';
+import { SendOutlined } from '@ant-design/icons';
 import { Button, Typography } from 'antd';
 import 'leaflet/dist/leaflet.css';
 import './ContactView.scss';
 
-const email = 'admin@admin.com';
+const members = [
+  {
+    name: 'Adel Mandanas',
+    email: 'mandanas.adel@gmail.com',
+  },
+  {
+    name: 'Celine Moredo',
+    email: 'camoredo@gmail.com',
+  },
+  {
+    name: 'Paco Pama',
+    email: 'pama.p.aa@m.titech.ac.jp',
+  },
+  {
+    name: 'Luis Flores',
+    email: 'luismigflores.21@gmail.com',
+  },
+  {
+    name: 'Luigi Del Rosario',
+    email: 'luigidr97@gmail.com',
+  },
+  {
+    name: 'Pio Fortuno III ',
+    email: 'plfortuno@up.edu.ph',
+  },
+];
 
 const ContactView: FunctionComponent = () => {
   const { Text, Title } = Typography;
 
-  const onEmailClick = () => {
+  const onEmailClick = (email: string) => {
     window.location.href = `mailto:${email}`;
   };
 
@@ -16,11 +42,20 @@ const ContactView: FunctionComponent = () => {
     <div className="Contact">
       <Title level={2}>Contact Us</Title>
 
-      <Text>You can send us an email at </Text>
-      <Button className="Contact-email" type="link" onClick={onEmailClick}>
-        {email}
-      </Button>
-      <Text>.</Text>
+      <Text>You can send an email to the members of the core team:</Text>
+      {members.map((m) => (
+        <div className="Contact-details">
+          <SendOutlined className="Contact-details-bullet" />
+          <Text>{m.name} - </Text>
+          <Button
+            className="Contact-details-email"
+            type="link"
+            onClick={() => onEmailClick(m.email)}
+          >
+            {m.email}
+          </Button>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/FaqView.tsx
+++ src/pages/FaqView.tsx
@@ -1,176 +1,86 @@
 import React, { FunctionComponent } from 'react';
-import { Typography } from 'antd';
+import { Button, Typography } from 'antd';
+import { Link } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
 import './FaqView.scss';
 
 const FaqView: FunctionComponent = () => {
-  const { Text, Title } = Typography;
+  const { Link: TextLink, Text, Title } = Typography;
 
   return (
     <div className="Faq">
       <Title level={2}>FAQ</Title>
 
-      <Title level={4}>What are we trying to solve?</Title>
+      <Title level={4}>What is Messages in Bottles?</Title>
       <Text>
-        There is concern worldwide that environmental issues we face today will
-        have an impact on future generations. Our challenge is to create a way
-        to communicate the importance of environmental responsibility to people
-        of all ages.
+        Messages in Bottles (WIP) is an online platform containing interactive
+        visualizations and information on coastal plastic waste using remote
+        sensing data from Sentinel-2. Inspired by the work of Dr. Biermen, Dr.
+        Clewley, Dr. Martinez-Vicente, and Dr. Topouzelis entitled “Finding
+        Plastic Patches in Coastal Waters using Optical Satellite Data”, this
+        project bridges the gap between their research and the general public in
+        the interest of providing a full picture on the severity of this problem
+        to everyone. The overarching goal of Messages in Bottles (WIP) is to
+        provide a hub for researchers in the field of marine environmental
+        science to display their work in a visual and easily digestible format
+        for the public to learn from.
       </Text>
-      <Title level={4}>What is &ldquo;Messages in Bottles&ldquo;?</Title>
+      <br />
+      <br />
       <Text>
-        One of the biggest threats to the world&lsquo;s oceans are floating
-        garbage patches; marine debris consisting of macro and microplastics
-        that through ocean currents can form large clusters in international
-        waters. The information on this problem is difficult to understand and
-        use for the general public and many researchers. With this, we developed
-        an easily digestible collection of information and visualization of
-        floating garbage patches (particularly the Great Pacific Garbage Patch)
-        using satellite data. Our solution aims to package complex remote
-        sensing data into a means of learning and tracking garbage patches in
-        open water to stimulate problem-solving in this area.
+        This project was developed as an entry to the NASA Space Apps 2020
+        Challenge by a group of six young researchers based in Metro Manila and
+        Japan.
       </Text>
-
-      <Title level={4}>How we addressed the challenge</Title>
+      <Title level={4}>
+        What was the research paper that heavily inspired this project?
+      </Title>
       <Text>
-        Messages in Bottles is an online platform containing interactive
-        visualizations and information on the Great Pacific Garbage Patch Marine
-        pollution accumulates as a consequence of waste from individual nations
-        leaking into international territory. This inherently means that while
-        these communities are at fault, there is no incentive for any large
-        scale cleanup efforts without the cooperation of the international
-        community.
+        The paper entitled “Finding Plastic Patches in Coastal Waters using
+        Optical Satellite Data” was written by Dr. Lauren Biermann, Dr. Daniel
+        Clewley, Dr. Victor Martinez-Vicente, and Dr. Konstantinos Topouzelis.
+        It was published on Nature, and you can read about it{' '}
       </Text>
-
-      <Title level={4}>How we developed the project</Title>
+      <TextLink href="https://www.nature.com/articles/s41598-020-62298-z">
+        here
+      </TextLink>
+      <Text>.</Text>
+      <br />
+      <br />
       <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
+        In their paper, they developed a novel Floating Debris Index to identify
+        suspected floating plastic waste in coastal waters. They then trained a
+        Naive Bayes classifier to determine plastic waste from suspected
+        clusters (since they can be mixed in with naturally occurring materials
+        like seaweed) with an accuracy of 86%. They applied their methods to
+        various regions of interest, some of which are visualized on this
+        platform. They showed for the first time that floating patches of
+        macroplastics are detectable in optical data from the European Space
+        Agency (ESA) Sentinel-2 satellites.
       </Text>
-
-      <Title level={4}>Question</Title>
+      <Title level={4}>Does this track the plastic waste in real time?</Title>
       <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
+        Not at the moment. Messages in Bottles only displays where suspected
+        floating plastic waste was detected in regions of interest. However, in
+        line with the overarching goal of bringing research to the public, this
+        platform was built for collaboration and expansion. Time-series
+        prediction, more regions of interest, and increased visual filters are
+        just some of the future plans of the team. Any marine environmental
+        science research is also welcome to have their data visualized for
+        public consumption.
       </Text>
 
-      <Title level={4}>Question</Title>
+      <Title level={4}>
+        I’m a researcher. How do I contribute to this project?
+      </Title>
       <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
+        Simply contact any of the members of the core team. You can find their
+        contact details{' '}
       </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
-
-      <Title level={4}>Question</Title>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        nisi felis, hendrerit sit amet volutpat at, tempus id enim. Aenean
-        elementum nisi sed urna mattis lacinia. In euismod metus non arcu
-        fermentum, id accumsan ex rutrum. Donec porttitor metus lectus, ut
-        feugiat nunc congue ultrices. Proin vitae metus lectus. Quisque risus
-        tortor, feugiat sit amet ligula at, accumsan dignissim est. Ut eu erat
-        vitae est pharetra commodo et sed purus. Donec aliquam neque ultricies
-        sapien malesuada ornare. Integer a auctor neque. Praesent at feugiat
-        nulla.
-      </Text>
+      <Button className="Faq-email" type="link">
+        <Link to="/contact">here</Link>
+      </Button>
+      <Text>.</Text>
     </div>
   );
 };
